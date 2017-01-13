6f72cc87ec7743cb996adf16b35ad9495841f764
Laure <laure.milian@gmail.com>
2017-01-13 20:03:18 +0100

    initial commit - structure page, ajout cdn semantic, testé

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
+</head>
+<body>
+
+
+</body>
+</html>
